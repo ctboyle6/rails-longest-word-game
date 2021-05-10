9880b0883e0b0d689d199ea0ebff847bc769e825
Chris Boyle <cthboyle6@gmail.com>
2021-05-10 14:21:49 -0400

    WIP: created controller with #new and #score pages. TODO: fix answer submission

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,14 @@
+class GamesController < ApplicationController
+  def new
+    alphabet = ('A'..'Z').to_a
+    @letters = []
+
+    10.times do
+      @letters << alphabet.sample
+    end
+  end
+
+  def score
+    raise
+  end
+end
